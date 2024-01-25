e41eac207005a8be17c1300c6ab53da3bb5dd97a
Sanjib Kumar Sen <mail@sanjibsen.com>
2024-01-25 17:22:14 +0600

    No need

--- src/app/api/stock/route.ts
+++ src/app/api/stock/route.ts
@@ -2,11 +2,6 @@ import { type NextRequest } from "next/server";
 import get_stock_price from "./parse";
 
 export async function GET(request: NextRequest) {
-  const sender = request.headers.get("referer");
-  console.log(sender, process.env.NEXT_PUBLIC_VERCEL_URL);
-  if (sender !== "http://localhost:3000/" && sender !== `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`) {
-    return new Response("Unauthorized", { status: 401 });
-  }
   const searchParams = request.nextUrl.searchParams;
   const company = searchParams.get("company");
   if (!company) {
