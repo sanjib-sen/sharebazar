36428b28c1e3e7fdf5fa9adc7d8d9aa2b6bb47f7
Sanjib Kumar Sen <mail@sanjibsen.com>
2024-01-25 17:14:53 +0600

    testing

--- src/app/api/stock/route.ts
+++ src/app/api/stock/route.ts
@@ -3,7 +3,7 @@ import get_stock_price from "./parse";
 
 export async function GET(request: NextRequest) {
   const sender = request.headers.get("referer");
-  console.log(sender);
+  console.log(sender, process.env.NEXT_PUBLIC_VERCEL_URL);
   if (sender !== "http://localhost:3000/" && sender !== `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`) {
     return new Response("Unauthorized", { status: 401 });
   }
