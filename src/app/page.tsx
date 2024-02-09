6de52731c8f5bcf65bdd9a2738ac53822e2eecd1
Sanjib Kumar Sen <mail@sanjibsen.com>
2024-02-10 02:22:41 +0600

    reduced dom width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Body } from "@/components/Body";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-3 gap-6 backdrop-blur-sm">
+    <main className="flex min-h-screen flex-col items-center justify-between p-6 gap-6 backdrop-blur-sm">
       <Body />
     </main>
   );
