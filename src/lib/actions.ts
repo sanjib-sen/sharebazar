76c7be06e7a4182929dccc7751d95c2ce0630cc8
Sanjib Kumar Sen <mail@sanjibsen.com>
2024-02-21 12:20:05 +0600

    updated packages and removed console log

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -11,7 +11,6 @@ export default async function get_stock_price(company: string) {
     const html = await response.text();
     const $ = load(html, { decodeEntities: false, scriptingEnabled: false });
     const data = $("td[width=25%]").first().html() as string;
-    console.log(data);
     if (data === "-") {
       return -1;
     }
